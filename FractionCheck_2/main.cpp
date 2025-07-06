828f98012df0a6548608fd8d4b7dae4acdda9611
Ada <Ada@PROBOOK>
2025-07-06 11:22:36 +0700

    Fraction lib

--- FractionCheck_2/main.cpp
+++ FractionCheck_2/main.cpp
@@ -5,4 +5,13 @@ void main()
 	setlocale(LC_ALL, "");
 	Fraction F1(2, 3, 5);
 	F1.print();
+
+	Fraction F2(3);
+	F2.print();
+
+	Fraction F3(3, 7);
+	F3.print();
+
+	Fraction F4 = F1 * F3;
+	F4.print();
 }
